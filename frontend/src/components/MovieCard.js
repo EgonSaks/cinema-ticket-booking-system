ca1223795058c9c6874c13a902f8c3040424d92f
Egon Saks <egonsaks@gmail.com>
2024-03-23 22:02:23 +0200

    refactor MovieCard, make formatDate as separate function

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import formatDate from '../utils/formatDate';
 
 const MovieCard = ({ movie }) => {
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(
-      date.getMonth() + 1
-    )
-      .toString()
-      .padStart(2, '0')}/${date.getFullYear()}`;
-    return formattedDate;
-  };
-
   return (
     <Link
       to={{ pathname: `/movie/${movie.id}`, state: { movie } }}
---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,11 @@
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(
+    date.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}/${date.getFullYear()}`;
+  return formattedDate;
+};
+
+export default formatDate;
