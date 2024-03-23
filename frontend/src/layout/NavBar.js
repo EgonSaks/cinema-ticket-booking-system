956070501e620343622f80d4237268d0fe6a9bc8
Egon Saks <egonsaks@gmail.com>
2024-03-23 20:33:38 +0200

    add logo

--- frontend/src/layout/NavBar.js
+++ frontend/src/layout/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     <div className='navbar flex justify-between py-5 border'>
       <div className='mx-16'>
         <a className='text-xl font-bold' href='/'>
-          Cinema
+          Cinema 🍿
         </a>
       </div>
     </div>
