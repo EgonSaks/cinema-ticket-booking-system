61a1b6255b3ee0ede6f0b12ab80c33618cb26c78
Egon Saks <egonsaks@gmail.com>
2024-03-23 07:37:23 +0200

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './layout/NavBar';
 
 function App() {
   return (
     <div className='App'>
-      
+      <NavBar />
     </div>
   );
 }
---
+++ frontend/src/layout/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <div className='navbar flex justify-between py-5 border'>
+      <div className='mx-16'>
+        <a className='text-xl font-bold' href='/'>
+          Cinema
+        </a>
+      </div>
+    </div>
+  );
+}
