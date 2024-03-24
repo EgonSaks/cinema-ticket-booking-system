8323cd37bb9f8a64337b1bcbf222a961c2aba7b7
Egon Saks <egonsaks@gmail.com>
2024-03-24 13:51:15 +0200

    change footer position

--- frontend/src/pages/MovieDetails.js
+++ frontend/src/pages/MovieDetails.js
@@ -47,7 +47,7 @@ const MovieDetails = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 h-[85.5vh]">
       <div className="max-w-5xl mx-auto">
         <div className="flex flex-wrap justify-center items-start">
           <div className="w-full md:w-1/2 lg:w-1/3 flex justify-center mb-8 md:mb-0">
