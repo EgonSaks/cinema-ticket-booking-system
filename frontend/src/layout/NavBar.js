deae58f9d50c912798d0e43a4bdf433d132c9872
Egon Saks <egonsaks@gmail.com>
2024-03-24 13:01:42 +0200

    make navbar responsive

--- frontend/src/layout/NavBar.js
+++ frontend/src/layout/NavBar.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default function NavBar() {
   return (
     <div>
-      <div className='navbar flex justify-between container mx-auto py-4 bg-red-500 rounded'>
-        <div className='mx-5'>
-          <a className='text-4xl font-bold border border-red-200 bg-white rounded px-2 py-1' href='/'>
+      <div className='navbar flex flex-col lg:flex-row justify-between container mx-auto py-4 bg-red-500 rounded'>
+        <div className='mx-5 mb-2 lg:mb-0'>
+          <a
+            className='text-4xl font-bold border border-red-200 bg-white rounded px-2 py-1'
+            href='/'
+          >
             Cinema 🍿
           </a>
         </div>
